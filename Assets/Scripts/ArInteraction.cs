77865f7fb5dca86ecc20f2bc8354b5568be50718
Liang_4399 <7624792+Liang2527@users.noreply.github.com>
2021-11-26 20:32:58 -0500

    Update 6

--- Assets/Scripts/ArInteraction.cs
+++ Assets/Scripts/ArInteraction.cs
@@ -11,6 +11,10 @@ public class ArInteraction : MonoBehaviour
     ManoGestureContinuous pinch;
     ManoGestureTrigger click;
     ManoGestureTrigger release;
+    ManoGestureContinuous pointer;
+
+    [SerializeField] Animator animator;
+    [SerializeField] Animation animation;
 
     int count = 0;
     // Start is called before the first frame update
@@ -22,29 +26,77 @@ public class ArInteraction : MonoBehaviour
         pinch = ManoGestureContinuous.HOLD_GESTURE;
         click = ManoGestureTrigger.CLICK;
         release = ManoGestureTrigger.RELEASE_GESTURE;
+        pointer = ManoGestureContinuous.POINTER_GESTURE;
+
+        animator = gameObject.GetComponent<Animator>();
+        animation = gameObject.GetComponent<Animation>();
+
     }
 
     // Update is called once per frame
 
     private void OnTriggerStay(Collider other)
     {
-        if (ManomotionManager.Instance.Hand_infos[0].hand_info.gesture_info.mano_gesture_continuous == pinch)
-        {
-            transform.Rotate(Vector3.up * Time.deltaTime * 50, Space.World);
+        //Debug.Log(" [OnTriggerStay] >>> [] >>> other.name:  " + other.gameObject.name);
+        if (gameObject.name == "PrefabChicken")
+        {   
+            Debug.Log("[ARinterection] >>> OnTriggerStay >>> PrefabChicken >>> ? ");
+            //transform.GetComponent<Animator>().enabled = true;
+            if (ManomotionManager.Instance.Hand_infos[0].hand_info.gesture_info.mano_gesture_continuous == pointer)
+            {
+                //chickenAnimator.Play("Idle_2", 0, 0.0f);
+                animation.Play("Idle_2");
+            }
+            else if (ManomotionManager.Instance.Hand_infos[0].hand_info.gesture_info.mano_gesture_trigger == click)
+            {
+                //Do Something
+                //chickenAnimator.Play("Eat", 0, 0.0f);
+                animation.Play("Eat");
+            }
+            else if (ManomotionManager.Instance.Hand_infos[0].hand_info.gesture_info.mano_gesture_continuous == pinch)
+            {
+                transform.Rotate(Vector3.up * Time.deltaTime * 50, Space.World);
+                animation.Play("Walk");
+            }
+            else if (ManomotionManager.Instance.Hand_infos[0].hand_info.gesture_info.mano_gesture_continuous == grab)
+            {
+                transform.parent = other.gameObject.transform;
+                animation.Play("Idle_2");
+            }
+            else if (ManomotionManager.Instance.Hand_infos[0].hand_info.gesture_info.mano_gesture_trigger == release)
+            {
+                transform.parent = null;
+                animation.Play("Idle_1");
+            }
         }
-        else if (ManomotionManager.Instance.Hand_infos[0].hand_info.gesture_info.mano_gesture_trigger == click)
+
+        else if(gameObject.name != "PrefabChicken")
         {
-            //Do Something
-            
-            //transform.parent = null;
+            if (ManomotionManager.Instance.Hand_infos[0].hand_info.gesture_info.mano_gesture_continuous == pointer)
+            {
+                //Do Something
+            }
+            else if (ManomotionManager.Instance.Hand_infos[0].hand_info.gesture_info.mano_gesture_trigger == click)
+            {
+                //Do Something
+                //ManomotionManager.Instance.Hand_infos[0].hand_info.tracking_info.poi.
+                GetComponent<Animation>().Play();
+            }
+            else if (ManomotionManager.Instance.Hand_infos[0].hand_info.gesture_info.mano_gesture_continuous == pinch)
+            {
+                transform.Rotate(Vector3.up * Time.deltaTime * 50, Space.World);
+            }
+            else if (ManomotionManager.Instance.Hand_infos[0].hand_info.gesture_info.mano_gesture_continuous == grab)
+            {
+                transform.parent = other.gameObject.transform;
+            }
+            else if (ManomotionManager.Instance.Hand_infos[0].hand_info.gesture_info.mano_gesture_trigger == release)
+            {
+                transform.parent = null;
+            }
         }
-        else if (ManomotionManager.Instance.Hand_infos[0].hand_info.gesture_info.mano_gesture_continuous == grab)
-        {
-            transform.parent = other.gameObject.transform;
-        }
-        else if (ManomotionManager.Instance.Hand_infos[0].hand_info.gesture_info.mano_gesture_trigger == release)
-        {
-            transform.parent = null;
-        }
+        else
+            Debug.Log(" [ARinterecation] >>> [onCollisionStay] >>> Nothiing to do");
+
     }
 }
